b69eccf98aeaa15fb49f7f9fc420a43f8a4d084d
DrasticArt <149271658+DrasticArt@users.noreply.github.com>
2023-10-28 16:35:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper-1",{
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: false,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+})
