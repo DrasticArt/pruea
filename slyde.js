0b74938ff51db053694544084e6944e24946d154
DrasticArt <149271658+DrasticArt@users.noreply.github.com>
2023-10-28 16:33:17 -0500

    Delete slyde.js

--- slyde.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper-1",{
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: false,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    }
-})
